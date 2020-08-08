d06531d2bbc839407e2b4a558247d847eb95388d
Robert McGrew <robbie2497@gmail.com>
2020-08-08 11:48:17 -0600

    fixing navbar

--- utahviews/client/src/components/Nav.js
+++ utahviews/client/src/components/Nav.js
@@ -2,79 +2,55 @@ import React from 'react';
 import { Link, useLocation } from "react-router-dom";
 
 function Nav() {
-    const location = useLocation();
-    return (
+  const location = useLocation();
+  return (
 
-
-        <ul className="nav nav-tabs">
-        <li className="nav-item">
-          <Link
-            to="/"
-            className={location.pathname === "/" ? "nav-link active" : "nav-link"}
-          >
-            Home
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container">
+        <a className="navbar-brand" href="/">UTAH VIEWS</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarResponsive">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item active">
+              <Link
+                to="/"
+                className={location.pathname === "/" ? "nav-link active" : "nav-link"}
+              >
+                Home
           </Link>
-        </li>
-        <li className="nav-item">
-          <Link
-            to="/search"
-            className={location.pathname === "/search" ? "nav-link active" : "nav-link"}
-          >
-            Search
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link
-            to="/"
-            className={location.pathname === "" ? "nav-link active" : "nav-link"}
-          >
-            Trails
-          </Link>
-          </li>
-          <li className="nav-item">
-            <Link
-            to="/"
-            className={location.pathname === "" ? "nav-link active" : "nav-link"}> Sign In
-
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/search"
+                className={location.pathname === "/search" ? "nav-link active" : "nav-link"}
+              >
+                Search
+        </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/login"
+                className={location.pathname === "/login" ? "nav-link active" : "nav-link"}
+              >
+                Log In
             </Link>
-        </li>
-        <li className="nav-item">
-          <Link
-          to="/"
-          className={location.pathname === "" ? "nav-link active" : "nav-link"}>Sign Up
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/signup"
+                className={location.pathname === "/signup" ? "nav-link active" : "nav-link"}
+              >
+                Sign Up
           </Link>
-        </li>
-      </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
 
-        
-        //Navigation
-        // <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-        //     <div className="container">
-        //         <a className="navbar-brand" href="#">UTAH VIEWS</a>
-        //         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-        //             <span className="navbar-toggler-icon"></span>
-        //         </button>
-        //         <div className="collapse navbar-collapse" id="navbarResponsive">
-        //             <ul className="navbar-nav ml-auto">
-        //                 <li className="nav-item active">
-        //                     <a className="nav-link" href="#">Home
-        //                         <span className="sr-only">(current)</span>
-        //                     </a>
-        //                 </li>
-        //                 <li className="nav-item">
-        //                     <a className="nav-link" href="#">About</a>
-        //                 </li>
-        //                 <li className="nav-item">
-        //                     <a className="nav-link" href="#">Sign Up</a>
-        //                 </li>
-        //                 <li className="nav-item">
-        //                     <a className="nav-link" href="#">Login</a>
-        //                 </li>
-        //             </ul>
-        //         </div>
-        //     </div>
-        // </nav>
-    );
+  );
 }
 
 export default Nav;
