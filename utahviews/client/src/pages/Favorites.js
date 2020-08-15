8aeae4d36d853a3d023080f13749e3a22803620a
Erin Otterstrom <hapiotter@gmail.com>
2020-08-15 13:10:57 -0600

    favs - needs login routes to work

--- utahviews/client/src/pages/Favorites.js
+++ utahviews/client/src/pages/Favorites.js
@@ -1,61 +1,29 @@
 import React, { useEffect, useState } from 'react';
 import '../App.css';
 import FavTable from '../components/FavTable';
-import API from '../utils/API';
+import API, { UserService } from '../utils/API';
 
 function Favorites() {
 
     const [trails, setTrails] = useState([]);
     const [favorites, setFavorites] = useState([])
-    const [favoriteOptions, setFavoriteOptions] = useState({
-      username: "",
-      id: ""
-    });
-  
+    // const [favoriteOptions, setFavoriteOptions] = useState({
+    //   username: "",
+    //   id: ""
+    // });
   
     useEffect(() => {
-      loadTrails()
-    }, [favoriteOptions])
-  
-    // useEffect(() => {
-    //   loadFavorites()
-    // }, [])
-  
-    function loadTrails() {
-      API.getTrails(favoriteOptions)
-        .then((res) =>
-          setTrails(res.data.trails)
-        )
-        .catch(err => console.log(err));
-      //API.getTrails().then((trails) => setTrails(trails.data.trails))
-    };
-  
-    // function loadFavorites() {
-    //   API.getFavorites(favoriteOptions)
-    //     .then((res) =>
-    //       setFavorites(res.data)
-    //     )
-    //     .catch(err => console.log(err));
-    // };
-  
-    // //searches for employees based on last name
-    // const handleChange = (event) => {
-    //   //updates filteredFavorites array
-    //   const filter = event.target.value.toLowerCase()
-    //   console.log(filter);
-    //   const result = favorites.filter(favorite => favorite.favorite.toLowerCase().includes(filter))
-    //   console.log(result[0]);
-    //   setFavoriteOptions(result[0]);
-    // }
-  
-  
-    // useEffect(() => console.log(trails), [trails])
-    // useEffect(() => console.log(favorites), [favorites])
+      UserService.getUser().then(res => UserService.populateFavorites(res.data._id).then(data => setFavorites(data)))
+    }, [])
+
+    useEffect(() => console.log(favorites), [favorites])
+    useEffect(() => console.log(trails), [trails])
   
     return (
       <>
         <div className="container">
-          {/* <FavTable trails={trails} favorites={favorites} handleChange={handleChange} /> */}
+          {/* <h1>{User.name}</h1> */}
+          {/* {favorites.map(x => <FavCard {...x} />)} */}
         </div>
       </>
     );
